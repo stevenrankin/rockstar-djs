ea9604ef9b6a7e8c24d8f4052fb624b2e932efc3
Steven J Rankin <rankin.steven@gmail.com>
2022-02-06 22:07:03 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -20,13 +20,7 @@ export default class MyDocument extends Document {
                     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
 
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap"
-                        rel="stylesheet"
-                    />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Vesper+Libre:wght@500&display=swap"
-                        rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Montserrat:ital,wght@1,600&display=swap" rel="stylesheet">
                     <link
                         rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
